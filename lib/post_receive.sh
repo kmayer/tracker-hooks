946a680b72ddbe0f5294c827a658008ca2de6055
Test Author <pair+ken+test@pivotallabs.com>
2012-12-11 09:10:09 -0800

    Refactoring, remove "Commit by" [#40639197]

--- lib/post_receive.sh
+++ lib/post_receive.sh
@@ -4,7 +4,7 @@ post_receive () {
 		xml_file=`tmp_file`
 		log_to_xml "$new_rev" > $xml_file
 		curl_to_tracker $xml_file
-		# rm $xml_file
+		rm $xml_file
 	done
 }
 
@@ -16,29 +16,23 @@ git_log_message () {
 }
 
 git_log_author () {
-	git log -1 --format="Commited by %an" $1
+	git log -1 --format="%an " $1
 }
 
 git_log_url () {
-	echo "http://example.com/project/commits/$1"
+	echo "http://atlas.mobileiron.com/fisheye/changelog/polaris?cs=$1"
 }
 
 log_to_xml () {
 	revision=$1
-	message=`git_log_message "$revision"`
-	author=`git_log_author "$revision"`
-	url=`git_log_url "$revision"`
 
-	sed -e "s%@@MESSAGE@@%$message%" \
-			-e "s%@@AUTHOR@@%$author%" \
-			-e "s%@@REVISION@@%$revision%" \
-			-e "s%@@URL@@%$url%"<<-SH
+	cat <<-SH
 <?xml version="1.0" encoding="UTF-8"?>
 <source_commit>
-  <message>@@MESSAGE@@</message>
-  <author>@@AUTHOR@@</author>
-  <commit_id>@@REVISION@@</commit_id>
-  <url>@@URL@@</url>
+  <message>`git_log_message "$revision"`</message>
+  <author>`git_log_author "$revision"`</author>
+  <commit_id>$revision</commit_id>
+  <url>`git_log_url "$revision"`</url>
 </source_commit>
 SH
 }
