6e5aec7125ff88e8ff3eb217e51b76e2fbbf376f
Kenneth Mayer <kmayer@bitwrangler.com>
2012-12-16 23:20:22 -0800

    Update README, allow for remote integration

--- README.md
+++ README.md
@@ -6,29 +6,60 @@ Tracker API: [https://www.pivotaltracker.com/help/api#scm_post_commit](https://w
 
 ## Installation
 
-Anywhere
+1. Anywhere
 
-	git clone git://github.com/kmayer/tracker-hooks.git
+		git clone git://github.com/kmayer/tracker-hooks.git
 		
-In your git repository's hook directory (i.e. `.git/hooks`)
+2. In your git repository's hook directory (i.e. `.git/hooks`)
 
-	cat > post-receive <<-SH
-	#!/usr/bin/env sh
+	  ```sh```
+		cat > post-receive <<-SH
+		#!/usr/bin/env sh
 	
-	# REQUIRED
-	export TRACKER_TOKEN=<your Pivotal Tracker API token>
+		# REQUIRED
+		export TRACKER_TOKEN=<your Pivotal Tracker API token>
 	
-	# optional
-	#   @@REVISION@@ will be replaced with the full git sha
-	export REPOS_URL="http://example.com/path/to/your/repos/@@REVISION@@"
+		# optional
+		#   @@REVISION@@ will be replaced with the full git sha
+		export REPOS_URL="http://example.com/path/to/your/repos/@@REVISION@@"
 	
-	exec /the/path/to/tracker-hooks/bin/post-receive
-	SH
+		exec /the/path/to/tracker-hooks/bin/post-receive
+		SH
+		```sh```
 	
 Make sure that `post-receive` is `chmod 0755`
 
 ## Testing
 
-	echo "x" `git rev-list -1 HEAD` "refs/heads/master" | ./git/hooks/post-receive
+I use [roundup](https://github.com/ohrite/roundup) for testing:
 
-You should see an XML response from the Tracker API. If your latest commit has a Tracker ID (e.g. [#12345]) it will try to post the note to the story.
+	  git submodule init
+	  git submodule update
+	  gem install bundler
+	  bundle
+	  vendor/roundup/roundup.sh
+
+Will run the test suite.
+  
+There is a [guard](http://github.com/hawx/guard-shell) file, too.
+  
+### Integration Testing
+  
+There's an integration test that will create a local & remote repository, and confirm that the commits were sent to pivotaltracker.com in the proper order.
+  
+If you set:
+  
+    TRACKER_TOKEN
+  	#and optionally
+    TRACKER_STORY
+  
+To your Pivotal Tracker API token and a test story, respectively, then you will see the test commits post updates to the story.
+
+## Contributing
+
+1. Fork on github
+2. Branch
+3. Write tests & code
+5. Push to github
+6. Pull request
+
--- spec/integration/post_receive_spec.sh
+++ spec/integration/post_receive_spec.sh
@@ -7,8 +7,9 @@ describe "post_receive integration"
 before ()
 {
 	REPOS_URL="http://repos.example.com/commit/@@REVISION@@"
-	TRACKER_TOKEN="let-me-in"
+	TRACKER_TOKEN=${TRACKER_TOKEN:-"let-me-in"}
 	TRACKER_HOOKS=`pwd`
+	TRACKER_STORY=${TRACKER_STORY:-"40639197"}
 }
 
 it_sends_a_commit_post_for_each_revision ()
@@ -23,13 +24,13 @@ it_sends_a_commit_post_for_each_revision ()
 	git init .
 	echo "file" > file1
 	git add file1
-	git commit -m 'Initial commit [#40639197]'
+	git commit -m "Initial commit [#${TRACKER_STORY}]"
 	echo "file" > file2
 	git add file2
-	git commit -m 'Second commit [#40639197]'
+	git commit -m "Second commit [#${TRACKER_STORY}]"
 	echo "changed" > file1
 	git add file1
-	git commit -m 'Third commit [#40639197]'
+	git commit -m "Third commit [#${TRACKER_STORY}]"
 
 	remote_repos=$test_root/remote
 	mkdir -p $remote_repos
@@ -39,13 +40,22 @@ it_sends_a_commit_post_for_each_revision ()
 	cat < /dev/null > $curls
 	cat > hooks/post-receive <<-SH
 	#!/bin/sh
-	export TRACKER_TOKEN=let-me-in
-	curl () {
-		echo "curl \$@" >> ${curls}
-	}
+	export TRACKER_TOKEN=${TRACKER_TOKEN}
+	SH
+	
+	if test "$TRACKER_TOKEN" = "let-me-in"; then
+		cat >> hooks/post-receive <<-SH
+		curl () {
+			echo "curl \$@" >> ${curls}
+		}
+		SH
+	fi
+	
+	cat >> hooks/post-receive <<-SH	
 	. $TRACKER_HOOKS/lib/post_receive.sh
 	post_receive
 	SH
+	
 	chmod +x hooks/post-receive
 	popd
 
@@ -53,13 +63,17 @@ it_sends_a_commit_post_for_each_revision ()
 	git push origin master
 	
 	git rm file1
-	git commit -am "Fourth commit [#40639197]"
+	git commit -am "Fourth commit [#${TRACKER_STORY}]"
 	
 	git rm file2
-	git commit -am "Fifth commit [#40639197]"
+	git commit -am "Fifth commit [#${TRACKER_STORY}]"
 	git push origin master
 	
-	git rev-list --reverse HEAD > $test_root/rev-list
-	awk '{print $4}' < $curls > $test_root/curl_count
-	cmp $test_root/rev-list $test_root/curl_count
+	if test "$TRACKER_TOKEN" = "let-me-in"; then
+		git rev-list --reverse HEAD > $test_root/rev-list
+		awk '{print $4}' < $curls > $test_root/curl_count
+		cmp $test_root/rev-list $test_root/curl_count
+	else
+		echo "YOU SHOULD CHECK ON TRACKER TO SEE IF THE POSTS MADE IT"
+	fi
 }
